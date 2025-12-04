073a7780fc1f709325cb78dce3e60fbc1d4271e5
Follish <90408443+Cheburek534@users.noreply.github.com>
2025-12-04 21:44:54 +0200

    Add comment to clarify random function behavior
    Added a comment in the random function to clarify its behavior.

--- 1-random.js
+++ 1-random.js
@@ -5,7 +5,8 @@ const random = (min, max) => {
     max = min;
     min = 0;
   }
-  return Math.floor(Math.random() * (max - min + 1) + min);
+  return Math.floor(Math.random() * (max - min + 1) + min); //округлює в меншу сторону
 };
-console.log(random(5,36));
-console.log(random(0, 20));
+
+console.log(random(5, 36));  
+console.log(random(0, 20));  

1718713d8170846a333bf7b103d55b475222fa77
Follish <90408443+Cheburek534@users.noreply.github.com>
2025-12-04 21:56:25 +0200

    Update 4-methods.js
    Added comments for clarity on parameter counts and result array.

--- 4-methods.js
+++ 4-methods.js
@@ -1,16 +1,21 @@
 'use strict';
 
 const iface = {
+ //різна кількість параметрів
   m1: x => [x],
+  
   m2: function (x, y) {
     return [x, y];
   },
+  
   m3(x, y, z) {
     return [x, y, z];
   }
 };
 
+//масив пар 
 const result = Object.entries(iface)
   .map(([key, fn]) => [key, fn.length]);
 
 console.log(result);
+
